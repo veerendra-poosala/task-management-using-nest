b0a47e80388e071d2a18a463e47db51c3287b162
veeru78866 <veeru78866@gmail.com>
2023-11-19 22:16:02 +0530

    Refactored methods with Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-
+//host=localhost port=5432 dbname=task-management user=postgres password=xxxxxxx sslmode=prefer connect_timeout=10
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'task_management',
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ]
 })
 export class AppModule {}
